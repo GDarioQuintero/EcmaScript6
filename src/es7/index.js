7b02134ff4d5d1bfa1fa10c7957eef4b7153c067
Dario <gdarioquintero@gmail.com>
2021-03-30 20:13:12 -0500

    Nuevas caracteristicas de EcmaScript7

---
+++ src/es7/index.js
@@ -0,0 +1,19 @@
+//VERSION DE ecmaScript7
+
+//Metodo include() - Valida si un valor se encuentra en un arreglo ------------------------------------------
+
+let number = [1, 4, 7, 5, 9];
+
+if (number.includes(2)){
+    console.log('Si se encuentra en numero');
+}
+else{
+    console.log('No se encuentra en numero');
+}
+
+//Como elevar a la potencia -------------------------------------------------------------------------------
+
+let base = 5;
+let exponente = 3;
+let result = base  ** exponente;
+console.log(result);
